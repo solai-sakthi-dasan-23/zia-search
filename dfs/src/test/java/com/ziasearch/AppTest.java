d3bb9496d09565573b7c04b58fea318d404c0d19
solai-sakthi-dasan-23 <solaysakthi.23@gmail.com>
2024-08-01 09:51:58 +0530

    feat:maven project creation

---
+++ dfs/src/test/java/com/ziasearch/AppTest.java
@@ -0,0 +1,20 @@
+package com.ziasearch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
